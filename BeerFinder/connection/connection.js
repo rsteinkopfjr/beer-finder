43e92c67a79ea46c3bbea541e2486ca4c6996aa0
rsteinkopfjr <rob.steinkopf@gmail.com>
2019-03-03 23:35:26 -0500

    Add connection.js

---
+++ BeerFinder/connection/connection.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql');
+
+var connection;
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else { 
+    connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "baconandeggs",
+    database: "beers_db"
+});
+};
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+
+});
+
+module.exports = connection;
